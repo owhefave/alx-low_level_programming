550a49e45767727e88c9b3d6985523386162eeb1
owhefave <owhefave@gmail.com>
2023-08-10 16:36:26 -0700

    using printf to output

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - To output some string 
+ * Return: return only zero for success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
