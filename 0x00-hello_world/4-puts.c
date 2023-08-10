37dc84f4e5b08c208eaa346911a45290701e0c10
owhefave <owhefave@gmail.com>
2023-08-10 16:23:52 -0700

    using the puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * main - This only prints out
- * Return - to return 0
-*/
+ * Return: to return 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
