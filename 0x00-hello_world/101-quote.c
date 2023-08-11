098bc9a6e71b7a3d926c6b64f407c74b6fdc9383
owhefave <owhefave@gmail.com>
2023-08-10 18:51:27 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+
 /**
  * main - This print using the file descriptor
  * Return: return 1 for success
@@ -7,5 +8,5 @@ int main(void)
 {
 	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(2, msg, sizeof(msg) - 1);
-	return 1;
+	return (1);
 }
