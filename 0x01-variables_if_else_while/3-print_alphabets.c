ccd4067d14dc91abea1d39c467b03a1ca5ab94af
owhefave <owhefave@gmail.com>
2023-08-12 14:50:02 -0700

    using putchar to output single characters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - printing alphabet in both upper and lower case
+ * Return: to return zero value
+ */
+int main(void)
+{
+	char alphabet;
+	for (alphabet= 'a'; alphabet<= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	for (alphabet= 'A'; alphabet<= 'Z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
