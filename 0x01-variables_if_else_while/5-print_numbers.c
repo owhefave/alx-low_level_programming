24442cf43755f38e6925a8ead895a7f6fc7b45c7
owhefave <owhefave@gmail.com>
2023-08-12 15:31:31 -0700

    printingg integers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - printing numbers
+ * Return: to return zero
+ */
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 0; numbers < 10; numbers++)
+	{
+		printf("%d", numbers);
+	}
+	printf("\n");
+	return (0);
+}
