0d4f0749c3185ddd8d98c9508bea4582d98e826b
owhefave <owhefave@gmail.com>
2023-08-12 14:54:16 -0700

    using putchar to output single characters

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,12 +5,13 @@
  */
 int main(void)
 {
-	char alphabet;
-	for (alphabet= 'a'; alphabet<= 'z'; alphabet++)
+	char alphabet
+		;
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 		putchar(alphabet);
 	}
-	for (alphabet= 'A'; alphabet<= 'Z'; alphabet++)
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
 	{
 		putchar(alphabet);
 	}
