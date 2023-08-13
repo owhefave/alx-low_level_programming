e37b8602cf63b807fe09d32323ae4cb5d54b4130
owhefave <owhefave@gmail.com>
2023-08-12 17:08:36 -0700

    hex numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - hexadecimal number
+ * Return: to always return zero
+ */
+int main(void)
+{
+	char number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		putchar(number);
+	}
+	for (number = 'a'; number <= 'f'; number++)
+	{
+		putchar(number);
+	}
+	putchar('\n');
+	return (0);
+}
