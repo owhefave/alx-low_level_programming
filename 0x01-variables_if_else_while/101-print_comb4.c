12cb9e01bde7a30e715947f7d1670f201cd1b3e2
owhefave <owhefave@gmail.com>
2023-08-14 15:15:14 -0700

    permutating 3 digits without repetition

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - to print possible combination
+ * Return: to always return zero
+ */
+int main()
+{
+	int num1;
+	int num2;
+	int num3;
+
+	for (num1 = 0; num1 <= 7; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= 8; num2++)
+		{
+			for (num3 = num2 + 1; num3 <= 9; num3++)
+			{
+				putchar(num1 + '0');
+				putchar(num2 + '0');
+				putchar(num3 + '0');
+				if (num1 != 7 || num2 != 8 || num3 != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+			
+
