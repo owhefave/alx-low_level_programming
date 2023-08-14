a426b8f6fe43b97ad9f49fcccd2560b6078971bd
owhefave <owhefave@gmail.com>
2023-08-14 05:51:57 -0700

    condition random numbers

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 	if (last_digit > 5)
 	{
 		printf("Last digit of %d is %d last_digit and"
-		"is greater than 5\n", n, last_digit);
+		" is greater than 5\n", n, last_digit);
 	}
 	else if (last_digit == 0)
 	{
