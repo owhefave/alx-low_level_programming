e6d7ca1fb59346ae484dc99c8ddfbb9e2ab23a0c
owhefave <owhefave@gmail.com>
2023-08-15 19:24:50 -0700

    checking integers sign

---
+++ 0x02-functions_nested_loops/5-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main - to print the sign of a number
+ * Return: Always zero
+ */
+int main(void)
+{
+	int print_sign(int n);
+	return(0);
+}
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - to print number signs
+ * @n: character whose symbol is to be checked
+ * Return: to return expected value
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+	_putchar('\n');
+}
+
