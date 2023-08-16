65ef03c43e3a76ebef39c76e4b5509ce2cd27110
owhefave <owhefave@gmail.com>
2023-08-16 15:04:12 -0700

    print absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - This function is to print absolute values
+ * @x: an integer
+ * Return: To always return zero
+ */
+int _abs(int x)
+{
+	r = _abs(x);
+
+	_putchar(r);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/6-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main - This prints the absolute value
+ * Return: Always return zero
+ */
+int main(void)
+{
+	int _abs(int);
+	return (0);
+}
