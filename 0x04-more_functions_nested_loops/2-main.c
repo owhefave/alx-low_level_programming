1461c4f7c07097c52f7cfa53c043bd82f7c4df7c
owhefave <owhefave@gmail.com>
2023-08-17 16:39:37 -0700

    to multiply two integers

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main - To multiply two integers
+ * Return: Always return 0
+ */
+int main(void)
+{
+	int mul(int a, int b);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: to return 0
+ */
+
+int mul(int a, int b);
+
+int mul(int a, int b)
+{
+	int multiply;
+
+	multiply = a * b;
+
+	return (multiply);
+}
