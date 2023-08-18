5efc10fff865d6ab9f8cae01073e23f76aaee5a0
owhefave <owhefave@gmail.com>
2023-08-17 18:33:57 -0700

    drawing a line

---
+++ 0x04-more_functions_nested_loops/6-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main - To draw a straight line
+ * Return: Always zero
+ */
+int main(void)
+{
+	void print_line(int n);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_line - to draw a straight line
+ * @n: to hold the line lenght
+ * Return: Always zero
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (n > 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
+
