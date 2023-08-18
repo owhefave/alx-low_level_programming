e335f3215ca4f1d435dcd1db9458eb578c1990ec
owhefave <owhefave@gmail.com>
2023-08-18 14:29:54 -0700

    debug a loop

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinitr loop incoming :(\n");
+
+	i = 0;
+
+	/*while (i < 10)*/
+	/*{*/
+	/*	putchar(i);*/
+	/*}*/
+
+	return (0);
+}
