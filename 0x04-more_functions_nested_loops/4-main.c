fb907311212fecea9b4e8c1d310f893fcd1d8c60
owhefave <owhefave@gmail.com>
2023-08-17 17:41:06 -0700

    dont print 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main - to print 0 to 9 except 2 and 4
+ * Return: Always return zero
+ */
+int main(void)
+{
+	void print_most_numbers(void);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_most_numbers - to print all integers except 2 and 4
+ * Return:to return 
+ */
+
+void print_most_numbers(void);
+
+void print_most_numbers(void)
+{
+	char x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		if (x != 2 && x != 4)
+		{
+			_putchar(x);
+		}
+	}
+	_putchar('\n');
+}
