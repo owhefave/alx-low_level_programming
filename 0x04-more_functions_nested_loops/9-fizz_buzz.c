39922406023e566584fbea472ed1334fd34444be
owhefave <owhefave@gmail.com>
2023-08-17 20:15:38 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - the fizzbuzz challenge
+ * Return: Always 0
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 1; number <= 100; number++)
+	{
+		if (number % 3 == 0 && number % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		} else if (number % 3 == 0)
+		{
+			printf("Fizz ");
+		} else if (number % 5 == 0)
+		{
+			printf("Buzz ");
+		} else
+		{
+			printf("%d ", number);
+		}
+	}
+	printf("\n");
+	return (0);
+}
