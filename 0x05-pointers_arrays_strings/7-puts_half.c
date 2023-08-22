177983ed842e4ae78c07e3260790b4fab1b2546d
owhefave <owhefave@gmail.com>
2023-08-22 15:31:33 -0700

    print secon half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,25 +10,26 @@ void puts_half(char *str)
 {
 	int len;
 	int x;
-	int odd;
 
 	len = 0;
 	while (str[len] != '\0')
 	{
 		len++;
 	}
-	for (x = len / 2; x < len; x++)
+	if (len % 2 == 0)
 	{
-		if (x / 2 == 0)
-		{
-			_putchar(str[x]);
-		}
-		else
-		{
-			odd = (len - 1) / 2;
-			_putchar(str[odd]);
-		}
+		x = len / 2;
 	}
+	else
+	{
+		x = (len - 1) / 2;
+	}
+	while (str[x] != '\0')
+	{
+		_putchar(str[x]);
+		x++;
+	}
+	_putchar('\n');
 }
 
 
