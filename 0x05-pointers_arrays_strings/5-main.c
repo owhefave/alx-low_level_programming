3ec74fdbe72d5416b663cc995acea4f42eb8345a
owhefave <owhefave@gmail.com>
2023-08-22 13:22:14 -0700

    reversing string

---
+++ 0x05-pointers_arrays_strings/5-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - to print string in reverse
+ * Return: to return zero
+ */
+
+int main(void)
+{
+	char s[10] = "My School";
+	
+	rev_string(s);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - to print strings in reverse
+ * @s: to hold the string
+ * Return: to return void
+ */
+
+void rev_string(char *s)
+{
+	int len;
+	int i;
+
+	len = 0;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
