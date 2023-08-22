94a934529fc199f9cdfd045492449f365d647a21
owhefave <owhefave@gmail.com>
2023-08-22 15:17:46 -0700

    updated

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,6 +10,7 @@ void puts_half(char *str)
 {
 	int len;
 	int x;
+	int odd;
 
 	len = 0;
 	while (str[len] != '\0')
@@ -18,7 +19,15 @@ void puts_half(char *str)
 	}
 	for (x = len / 2; x < len; x++)
 	{
-		_putchar(str[x]);
+		if (x / 2 == 0)
+		{
+			_putchar(str[x]);
+		}
+		else
+		{
+			odd = (len - 1) / 2;
+			_putchar(str[odd]);
+		}
 	}
 }
 
