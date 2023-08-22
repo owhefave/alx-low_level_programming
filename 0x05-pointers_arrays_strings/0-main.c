77d16ef23f481c9169b63b3759baedc1d31b002e
owhefave <owhefave@gmail.com>
2023-08-22 02:59:22 -0700

    to reset a pointer value to 98

---
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - too return a value back to the function value
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	void reset_to_98(int *n);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - to reset the value of a pointer to 98
+ * @n: variable to hold the value
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
