e3846dbefecec9b5ebbdc56eb97a1594ee011534
owhefave <owhefave@gmail.com>
2023-08-30 07:15:20 -0700

    recursion that print string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - recursion to print string
+ * @s: pointer to hold the variable
+ * Return: returns void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		return (_puts_recursion(s + 1));
+	}
+	_putchar('\n');
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,6 @@
+# 0x08. C - Recursion
+
+1. What is recursion
+1. How to implement recursion
+1. In what situations you should implement recursion
+1. In what situations you shouldn’t implement recursion
