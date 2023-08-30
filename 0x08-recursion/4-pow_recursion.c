e6be226ac28141a6cdac00a91bb8c25d6e8a1402
owhefave <owhefave@gmail.com>
2023-08-30 14:05:02 -0700

    square of a number

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _pow_recursion - to calculate power of a number
+ * @x: the variable holding the base
+ * @y: the variable holding the index
+ * Return: return value of x raised to power y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y-1));
+	}
+}
