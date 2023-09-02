3a4dd0fd7e2d36eab6386c941412c60a04e2f2cf
owhefave <owhefave@gmail.com>
2023-09-02 04:24:06 -0700

    print its own name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - To print that print its own name
+ * @argc: to hold the count of command line arguement
+ * @argv: to hold the variable itself
+ * Return: return 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+# 0x0A. C - argc, argv
+
+About passing command line arguement
