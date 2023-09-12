818f3146197de783541b6568b77bbaadee1b4b5f
owhefave <owhefave@gmail.com>
2023-09-12 04:26:02 -0700

    initialize a struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
-
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct dog - dog basic info
@@ -14,5 +15,7 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
